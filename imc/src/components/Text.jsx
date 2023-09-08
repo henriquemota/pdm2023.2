c86cdb7b264ebb3a8196ba970732f6ed3392883a
henriquemota <mota.henrique@gmail.com>
2023-09-08 08:15:00 -0300

    calculadora de imc funcional

--- imc/src/components/Text.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Styles from '../styles/Styles'
-
-export default function Text({ text }) {
-  return (
-    <Text style={Styles.message}>{text}</Text>
-  )
-}
--- imc/src/components/Title.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Text } from 'react-native'
-import { Styles } from '../styles/Styles'
-
-export default function Title({ text }) {
-  return (
-    <Text style={Styles.title}>{text}</Text>
-  )
-}
