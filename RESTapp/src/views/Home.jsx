3c2e48c914b14ca62eab4ba55168c0932a86d6b2
henriquemota <mota.henrique@gmail.com>
2023-09-25 19:40:35 -0300

    implementacoa do estilo

--- RESTapp/src/views/Home.jsx
+++ RESTapp/src/views/Home.jsx
@@ -1,7 +1,48 @@
 import { useNavigation } from '@react-navigation/native'
-import { ActivityIndicator, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import { useCEP } from '../hooks'
 import { useState } from 'react'
+import { ActivityIndicator, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { useCEP } from '../hooks'
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 8
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 4,
+    padding: 8,
+    marginVertical: 4,
+    flex: 1,
+  },
+  inputGroup: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 2,
+    justifyContent: 'space-between'
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 4,
+    padding: 6,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    alignSelf: 'center'
+  },
+  card: {
+    padding: 16,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 8,
+    backgroundColor: '#fff',
+    marginVertical: 16
+  },
+})
 
 const Home = () => {
   const [animating, setAnimating] = useState(false)
@@ -11,30 +52,34 @@ const Home = () => {
   const { getAddress } = useCEP()
 
   return (
-    <View>
+    <View style={Styles.container}>
       <Text>Informe seu CEP</Text>
-      <TextInput
-        style={{ padding: 8, border: 1, borderColor: '#000' }}
-        placeholder='Informe o CEP'
-        onChangeText={text => setCEP(text)}
-      />
-      <TouchableOpacity
-        onPress={() => {
-          setAnimating(true)
-          setTimeout(() => {
-            getAddress(cep)
-              .then(r => setAddress(r))
-              .catch(() => setAddress(null))
-              .finally(() => setAnimating(false))
-          }, 1500)
-        }}
-      >
-        <Text>Localizar</Text>
-        <ActivityIndicator animating={animating} />
-      </TouchableOpacity>
+      <View style={Styles.inputGroup}>
+        <TextInput
+          style={Styles.textInput}
+          placeholder='Informe o CEP'
+          onChangeText={text => setCEP(text)}
+        />
+        <TouchableOpacity
+          disabled={animating}
+          style={Styles.button}
+          onPress={() => {
+            setAnimating(true)
+            setTimeout(() => {
+              getAddress(cep)
+                .then(r => setAddress(r))
+                .catch(() => setAddress(null))
+                .finally(() => setAnimating(false))
+            }, 1500)
+          }}
+        >
+          <Text style={Styles.buttonText}>Localizar</Text>
+          <ActivityIndicator animating={animating} />
+        </TouchableOpacity>
+      </View>
 
       {address &&
-        <View>
+        <View style={Styles.card}>
           <Text>cep: {address.cep}</Text>
           <Text>logradouro: {address.logradouro} </Text>
           <Text>complemento: {address.coplemento}</Text>
@@ -47,7 +92,6 @@ const Home = () => {
           <Text>siafi: {address.safi} </Text>
         </View>
       }
-
     </View>
   )
 }
