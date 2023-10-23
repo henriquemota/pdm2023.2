33d0c870b96a6f23f06c260783c6c631d04767e9
henriquemota <mota.henrique@gmail.com>
2023-10-23 19:21:46 -0300

    implementacao

--- AsyncStorageApp/src/views/Home.jsx
+++ AsyncStorageApp/src/views/Home.jsx
@@ -1,23 +1,20 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import React, { useState } from 'react'
-import { Button, Text, TextInput, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Button, TextInput, View } from 'react-native'
 
 const Home = () => {
-  const [list, setList] = useState([])
-  const [data, setData] = useState({ nome: 'henrique', idade: '49' })
+  const [data, setData] = useState({ nome: '', idade: '' })
+
+  useEffect(() => {
+    AsyncStorage
+      .getItem('pessoas')
+      .then(e => setData(JSON.parse(e)))
+
+  }, [])
 
   const gravarDados = () => {
     AsyncStorage
       .setItem('pessoas', JSON.stringify(data))
-      .then(e => console.log('deu certo'))
-      .catch(e => console.log('erro'))
-      .finally(() => console.log('aqui'))
-  }
-
-  const lerDados = () => {
-    AsyncStorage
-      .getItem('pessoas')
-      .then(e => setList([...list, JSON.parse(e)]))
   }
 
   return (
@@ -26,7 +23,7 @@ const Home = () => {
     >
       <TextInput
         style={{ borderWidth: 1, borderColor: '#000', padding: 16, margin: 6 }}
-        value={data?.nome}
+        value={data.nome}
         onChangeText={text => setData({ ...data, nome: text })}
       />
       <TextInput
@@ -39,15 +36,7 @@ const Home = () => {
         title='Salvar'
         onPress={gravarDados}
       />
-      <Button
-        title='Ler dados'
-        onPress={lerDados}
-      />
-      {list.map((el, ix) =>
-        <View kei={ix}>
-          <Text>{el.nome} - {el.idade}</Text>
-        </View>
-      )}
+
     </View>
   )
 }
